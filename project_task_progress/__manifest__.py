7e4086a172197dc234ab7842d049b8daff3164d0
Matthwhy <marquesmathw@gmail.com>
2024-01-16 12:40:54 -0300

    [ADD] project_task_progress: add new module

---
+++ project_task_progress/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Project Task Progress',
+    'summary': """
+        Manage current task advancement""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'http://github.com/escodoo/project-addons.git',
+    'depends': ['base','project'
+    ],
+    'data': [
+        'views/project_task.xml',
+    ],
+}
