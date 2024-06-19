94b9abf56319c10f27716c54c0b6e5889b93be01
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-06-19 14:23:24 -0300

    [FIX] project_sla: remove field start_date_sla and return cron in manifest

--- project_sla/__manifest__.py
+++ project_sla/__manifest__.py
@@ -17,7 +17,7 @@
         "views/project_project.xml",
         "security/ir.model.access.csv",
         # TODO: os métodos utilizados pelo CRON precisam ser revisados
-        # "data/ir_cron_data.xml",
+        "data/ir_cron_data.xml",
     ],
     "demo": [],
 }
